8fd98a2d8a556dffbb220db03f09aa96230603b2
daisuke sasaki <gerund25495641@gmail.com>
2021-02-22 23:56:08 +0900

    bingo game complete

--- js/main.js
+++ js/main.js
@@ -1,52 +1,43 @@
 'use strict';
 
-
 {
-//  const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-
-/* 関数 */
-function createColumn(col){
+  function createColumn(col) {
     const source = [];
-    for (let i = 0; i < 15; i++){
-      // 1 ... 15
-      // source[i] = i + 1 + 15 * 0;
-      // 16 ... 30
-      // source[i] = i + 1 + 15 * 1;
-      // 31 ... 45
-      // source[i] = i + 1 + 15 * 2;
+    for (let i = 0; i < 15; i++) {
+      source[i] = i + 1 + 15 * col;
     }
-    
+  
+    const column = [];
+    for (let i = 0; i < 5; i++) {
+      column[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    }
+
+    return column;
+  }
+
+  function createColumns() {
+    const columns = [];
+    for (let i = 0; i < 5; i++) {
+      columns[i] = createColumn(i);
+    }
+    columns[2][2] = 'FREE';
+    return columns;
+  }
+
+  function renderBingo(columns) {
+    for (let row = 0; row < 5; row++) {
+      const tr = document.createElement('tr');
+      for (let col = 0; col < 5; col++) {
+        const td = document.createElement('td');
+        td.textContent = columns[col][row];
+        tr.appendChild(td);
+      }
+      document.querySelector('tbody').appendChild(tr);
+    }
+  }
+
+  const columns = createColumns();
+  renderBingo(columns);
 }
-
-
-
-
-
-//  Math.floor(Math.random() * (14+1))
-//  Math.floor(Math.random() * source.length)
-
- const b = [];
-//  b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-//  b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-//  b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-//  b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-//  b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-
-for (let i = 0; i < 5; i++){
-    b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-}
-
- console.log(b);
-
-}
-
-const columns = [];
-columns[0] = createColumn(0);
-columns[1] = createColumn(0);
-columns[2] = createColumn(0);
-columns[3] = createColumn(0);
-columns[4] = createColumn(0);
-
-
 
 
